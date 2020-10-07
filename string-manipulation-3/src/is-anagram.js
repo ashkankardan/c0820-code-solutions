2ccdfb8443756bbf86d48a6274cbfab25523a9c6
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-07 17:11:41 +0000

    work in progress

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+
+const isAnagram = (firstString, secondString) => {
+  let anagram = false;
+  const tempArr = [];
+  const tempArr2 = [];
+
+  for (let i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      tempArr.push(firstString[i]);
+    }
+  }
+
+  for (let i = 0; i < secondString.length; i++) {
+    if (secondString[i] !== ' ') {
+      tempArr2.push(secondString[i]);
+    }
+  }
+
+  console.log(tempArr);
+  console.log(tempArr2);
+  if (tempArr.length === tempArr2.length) {
+    for (let i = 0; i < tempArr2.length; i++) {
+      for (let j = 0; j < tempArr.length; j++) {
+        if (tempArr2[i] === tempArr[j]) {
+          anagram = true;
+          tempArr.splice(j, 1);
+        }
+      }
+      if (tempArr2.length > 0) {
+        anagram = false;
+      }
+    }
+  }
+
+  return anagram;
+};
