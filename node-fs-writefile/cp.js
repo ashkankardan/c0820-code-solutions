90903c38f3d8dd290310ceaf6621e0a8c09e6f54
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-09 18:55:36 +0000

    completed Node fs writefile 3

---
+++ node-fs-writefile/cp.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+
+const fs = require('fs');
+
+const fromFile = process.argv[2];
+const toFile = process.argv[3];
+
+fs.readFile(fromFile, 'utf8', (err, data) => {
+  if (err) throw err;
+  const content = data;
+
+  fs.writeFile(toFile, content, err => {
+    if (err) throw err;
+    console.log('successful!!');
+  });
+});
