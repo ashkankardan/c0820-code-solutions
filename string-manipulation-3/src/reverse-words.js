f8a7c4d219dea3a7122299f0d2ffe7c0738a2ef1
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-09 02:06:24 +0000

    work in progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -3,8 +3,8 @@
 const reverseWords = string => {
   const wordsArr = string.split(' ');
   const newArr = [];
-  const reveresedStr = '';
-  for (let i = 0; i < wordsArr; i++) {
+  let reveresedStr = '';
+  for (let i = 0; i < wordsArr.length; i++) {
     let reversedWord = '';
     for (let j = wordsArr[i].length - 1; j >= 0; j--) {
       reversedWord += wordsArr[i][j];
@@ -14,5 +14,5 @@ const reverseWords = string => {
   for (let k = 0; k < newArr.length; k++) {
     reveresedStr += `${newArr[k]} `;
   }
-  return reveresedStr;
+  return reveresedStr.slice(0, -1);
 };
