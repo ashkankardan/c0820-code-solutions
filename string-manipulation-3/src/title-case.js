fc35bf1bffd94a9677b243bf9694f6433ffd83bf
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-09 21:53:57 +0000

    work in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,5 +1,19 @@
 /* eslint-disable no-unused-vars */
 
 const titleCase = string => {
+  let titleArr = [];
+  let subTitleArr = [];
+  if (string.split(':')[1] !== undefined) {
+    titleArr = string.split(':')[0];
+    subTitleArr = string.split(':')[1];
+  } else {
+    subTitleArr = string.split(':')[0];
+  }
+
+  for (let i = 0; i < titleArr.length; i++) {
+    const tempWordStr = string[0].toUpperCase() + string.slice(1);
+
+    // -------------->
+  }
 
 };
