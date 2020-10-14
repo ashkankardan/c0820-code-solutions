0399586a96ae3bea644e5e8270e66d95b410cdc2
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-14 19:12:23 +0000

    completed express post json

---
+++ express-post-json/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const grades = [];
+let nextId = 1;
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.post('/api/grades', (req, res, next) => {
+  newEntry(req.body);
+  res.status(201).end();
+  next();
+});
+
+const newEntry = data => {
+  data.id = nextId;
+  grades.push(data);
+  nextId++;
+};
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000');
+});
