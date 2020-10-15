47b1c89ea900493850df5084b1fa140614b68f79
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-15 16:52:24 +0000

    completed express delete

---
+++ express-delete/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const studentData = [
+  {
+    id: 1,
+    name: 'Ashkan Kardan',
+    course: 'JavaScript',
+    grade: 99
+  },
+  {
+    id: 2,
+    name: 'Jamie Hizenberg',
+    course: 'Chemistry',
+    grade: 100
+  }
+];
+
+app.get('/api/grades', (req, res) => {
+  res.json(studentData);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  studentData.splice((req.params.id - 1), 1);
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000');
+});
