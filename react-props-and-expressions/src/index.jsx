38f4d13d6eb57d58a84e0a52ce16f43bf3a3fffc
Ashkan Kardan <ashkankardan14@gmail.com>
2020-10-20 22:48:04 +0000

    completed React Props and Expressions

---
+++ react-props-and-expressions/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton(props) {
+  return <button>{props.text}</button>;
+}
+
+const rootEl = document.querySelector('#root');
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+ReactDOM.render(element, rootEl);
